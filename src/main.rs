f6c7aa47045e0d5c4b77be4bbcf2289c7f76ee03
wangzhaohan2910 <wangcf3000@sina.com>
2026-01-10 07:42:01 +0800

    紑燥

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,39 @@ use axum::{
     Form, Router,
     http::{HeaderMap, HeaderValue},
     response::{Html, IntoResponse},
-    routing::{get, post},
+    routing::get,
 };
 use device_query::{DeviceEvents, DeviceEventsHandler};
+use enigo::{Enigo, Keyboard, Settings};
 use rdev::listen;
 use serde::Deserialize;
+use shlex::split;
 use std::{
     env::args,
     io::Cursor,
     sync::{Arc, Mutex},
     time::Duration,
 };
+use std::collections::HashMap;
+use std::iter::Map;
 use tokio::{main, net::TcpListener, process::Command, spawn};
 use xcap::{Monitor, image::ImageFormat};
 #[derive(Deserialize)]
 struct Frm {
-    area: String,
+    choice: String,
+    text: String,
 }
 #[main]
 async fn main() {
     let mut arg = args();
     arg.next();
     let lock = Arc::new(Mutex::new(String::new()));
-    let lock1 = lock.clone();
     let lock2 = lock.clone();
     let lock3 = lock.clone();
     let lck = Arc::new(Mutex::new(String::new()));
-    let lck1 = lck.clone();
     let lck2 = lck.clone();
     let handler = DeviceEventsHandler::new(Duration::ZERO).unwrap();
+    let enigo = Arc::new(Mutex::new(Enigo::new(&Settings::default()).unwrap()));
     let _down =
         handler.on_key_down(move |key| *lock2.lock().unwrap() += &(key.to_string() + "&#x2193; "));
     let _up =
@@ -52,38 +56,43 @@ async fn main() {
             .unwrap();
         }
     });
+    let mut head = HeaderMap::new();
+    head.insert(
+        "Cache-Control",
+        HeaderValue::from_static("no-store, no-cache, must-revalidate, max-age=0"),
+    );
+    head.insert("Expires", HeaderValue::from_static("-1"));
+    head.insert("Pragma", HeaderValue::from_static("no-cache"));
     axum::serve(
         TcpListener::bind(String::from("0.0.0.0:") +
             &arg.next().unwrap_or(String::from("1145"))).await.unwrap(),
         Router::new()
-    .route("/", get(async move || {
-        ({
-            let mut head = HeaderMap::new();
-            head.insert(
-                "Cache-Control",
-                HeaderValue::from_static("no-store, no-cache, must-revalidate, max-age=0"),
-            );
-            head.insert("Expires", HeaderValue::from_static("-1"));
-            head.insert("Pragma", HeaderValue::from_static("no-cache"));
-            head
-        }, Html(
+    .route("/", get(async move |frm: Form</*Option<Frm>*/ HashMap<String, String> >| { //参数Map
+        println!("{:#?}", frm);
+        return "Hello";/*
+        if let Form(Some(Frm{choice, text})) = frm {
+            match &choice[..] {
+                "cmd" => {
+                    let parts: Vec<String> = split(&text).unwrap();
+                    Command::new(&parts[0]).args(&parts[1..]).spawn().unwrap();
+                },
+                "code" => (), // TODO: code
+                "eni" => (), // TODO: enigo
+                "rdev" => (), // TODO: rdev
+                "txt" => enigo.lock().unwrap().text(&text).unwrap(),
+                "clr" => {
+                    lock.lock().unwrap().clear();
+                    lck.lock().unwrap().clear();
+                },
+                _ => ()
+            }
+        }
+        (head, Html(
             String::from(
-                r#"<!DOCTYPE html><html><body><form action="/cmd"method="post"><textarea name="area"></textarea><input type="submit"value="提交"/></form><a href="/clear">清空</a><pre style="white-space:pre-wrap;">"#,
-            ) + lock1.lock().unwrap().as_str() + "<hr>" + lck1.lock().unwrap().as_str()
+                r#"<!DOCTYPE html><html><body><form action="/cmd"><select name="choice"><option value="cmd">命令</option><option value="code">键盘码</option><option value="eni">enigo按键</option><option value="rdev">rdev按键</option><option value="txt">输入</option><option value="clr">清空</option></select><input name="text"/><input type="submit"/></form><pre style="white-space:pre-wrap;">"#,
+            ) + lock.lock().unwrap().as_str() + "<hr>" + lck.lock().unwrap().as_str()
                 + r#"</pre><img src="screen.bmp"/><script>setInterval(()=>{document.querySelector("img").src="/screen.bmp?"+Date.now()},160);</script></body></html>"#,
-        )).into_response()
-    })) .route("/cmd", post(async |Form(Frm { mut area })| {
-        area = String::from("cmd /c ") + &area;
-        let parts: Vec<&str> = area.split_whitespace().collect();
-        Command::new(parts[0]).args(&parts[1..]).spawn().unwrap();
-        Html(
-            r#"<!DOCTYPE html><html><body><script>window.location.href="/"</script></body></html>"#,
-        )
-    })) .route("/clear", get(async move || {
-        lock.lock().unwrap().clear(); lck.lock().unwrap().clear();
-        Html(
-            r#"<!DOCTYPE html><html><body><script>window.location.href="/"</script></body></html>"#,
-        )
+        )).into_response()*/
     })) .route("/screen.bmp", get(async || {
         let mut buf = Cursor::new(Vec::new());
         Monitor::all().unwrap()[0].capture_image().unwrap()

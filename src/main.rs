e3f5925d72ca88100b007eb62006bb075057cfb4
wangzhaohan2910 <wangcf3000@sina.com>
2026-01-10 16:54:33 +0800

    Good

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@
 use axum::{
     Form, Router,
     http::{HeaderMap, HeaderValue},
-    response::{Html, IntoResponse},
+    response::IntoResponse,
     routing::get,
 };
 use device_query::{DeviceEvents, DeviceEventsHandler};
 use enigo::{Enigo, Keyboard, Settings};
 use rdev::listen;
-use serde::Deserialize;
+use serde::{
+    Deserialize, Deserializer,
+    de::{MapAccess, Visitor},
+};
 use shlex::split;
 use std::{
     env::args,
@@ -16,14 +19,36 @@ use std::{
     sync::{Arc, Mutex},
     time::Duration,
 };
-use std::collections::HashMap;
-use std::iter::Map;
 use tokio::{main, net::TcpListener, process::Command, spawn};
 use xcap::{Monitor, image::ImageFormat};
-#[derive(Deserialize)]
+#[derive(Debug)]
 struct Frm {
     choice: String,
     text: String,
+}
+struct Visit {}
+impl<'a> Visitor<'a> for Visit {
+    type Value = Frm;
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("choice and text")
+    }
+    fn visit_map<M: MapAccess<'a>>(self, mut access: M) -> Result<Self::Value, M::Error> {
+        let mut choice = String::new();
+        let mut text = String::new();
+        while let Some((k, v)) = access.next_entry()? {
+            match k {
+                "choice" => choice = v,
+                "text" => text = v,
+                _ => (),
+            }
+        }
+        Ok(Frm { choice, text })
+    }
+}
+impl<'a> Deserialize<'a> for Frm {
+    fn deserialize<D: Deserializer<'a>>(deserializer: D) -> Result<Self, D::Error> {
+        deserializer.deserialize_map(Visit {})
+    }
 }
 #[main]
 async fn main() {
@@ -63,36 +88,33 @@ async fn main() {
     );
     head.insert("Expires", HeaderValue::from_static("-1"));
     head.insert("Pragma", HeaderValue::from_static("no-cache"));
+    head.insert("Content-Type", HeaderValue::from_static("text/html;charset=utf-8"));
     axum::serve(
         TcpListener::bind(String::from("0.0.0.0:") +
             &arg.next().unwrap_or(String::from("1145"))).await.unwrap(),
         Router::new()
-    .route("/", get(async move |frm: Form</*Option<Frm>*/ HashMap<String, String> >| { //参数Map
-        println!("{:#?}", frm);
-        return "Hello";/*
-        if let Form(Some(Frm{choice, text})) = frm {
-            match &choice[..] {
-                "cmd" => {
-                    let parts: Vec<String> = split(&text).unwrap();
-                    Command::new(&parts[0]).args(&parts[1..]).spawn().unwrap();
-                },
-                "code" => (), // TODO: code
-                "eni" => (), // TODO: enigo
-                "rdev" => (), // TODO: rdev
-                "txt" => enigo.lock().unwrap().text(&text).unwrap(),
-                "clr" => {
-                    lock.lock().unwrap().clear();
-                    lck.lock().unwrap().clear();
-                },
-                _ => ()
-            }
+    .route("/", get(async move |frm: Form<Frm>| {
+        let Form(Frm { choice, text }) = frm;
+        match &choice[..] {
+            "cmd" => {
+                let parts: Vec<String> = split(&text).unwrap();
+                Command::new(&parts[0]).args(&parts[1..]).spawn().unwrap();
+            },
+            "code" => (), // TODO: code
+            "eni" => (), // TODO: enigo
+            "rdev" => (), // TODO: rdev
+            "txt" => enigo.lock().unwrap().text(&text).unwrap(),
+            "clr" => {
+                lock.lock().unwrap().clear();
+                lck.lock().unwrap().clear();
+            },
+            _ => ()
         }
-        (head, Html(
-            String::from(
-                r#"<!DOCTYPE html><html><body><form action="/cmd"><select name="choice"><option value="cmd">命令</option><option value="code">键盘码</option><option value="eni">enigo按键</option><option value="rdev">rdev按键</option><option value="txt">输入</option><option value="clr">清空</option></select><input name="text"/><input type="submit"/></form><pre style="white-space:pre-wrap;">"#,
+        (head, String::from(
+                r#"<!DOCTYPE html><html><body><form action="/"><select name="choice"><option value="cmd">命令</option><option value="code">键盘码</option><option value="eni">enigo按键</option><option value="rdev">rdev按键</option><option value="txt">输入</option><option value="clr">清空</option></select><input name="text"/><input type="submit"/></form><pre style="white-space:pre-wrap;">"#,
             ) + lock.lock().unwrap().as_str() + "<hr>" + lck.lock().unwrap().as_str()
-                + r#"</pre><img src="screen.bmp"/><script>setInterval(()=>{document.querySelector("img").src="/screen.bmp?"+Date.now()},160);</script></body></html>"#,
-        )).into_response()*/
+                + r#"</pre><img src="screen.bmp"/><script>setInterval(()=>document.querySelector("img").src="/screen.bmp?"+Date.now(),160);</script></body></html>"#,
+        ).into_response()
     })) .route("/screen.bmp", get(async || {
         let mut buf = Cursor::new(Vec::new());
         Monitor::all().unwrap()[0].capture_image().unwrap()
